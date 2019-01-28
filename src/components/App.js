e128eb264f0eaefc434ec4962ffc4f0c84ea9cfe
Roger KNL <roger.knl@gmail.com>
2019-01-28 22:12:49 +0100

    feat/noteList

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,18 @@ import NoteList from './NoteList';
 import './App.css';
 
 class App extends Component {
+  //setting inital state
+  constructor(props){
+    super(props);
+    this.state = { 
+      notes: [
+        { header:
+          { previous_hash: null, hash: null },
+          data: { title: null, description: null, created_at: null }
+        }
+      ]
+    };
+  }
   renderAddOrModify(){
     if (false){
       return <NoteModify></NoteModify>
@@ -17,7 +29,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NoteList></NoteList>
+        <NoteList notes={this.state.notes}></NoteList>
         {this.renderAddOrModify()}
       </div>
     );
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -3,11 +3,42 @@ import React, { Component } from 'react';
 import './NoteList.css';
 
 class NoteList extends Component {
+  renderNotes(){
+    //if notes is only 1 intem length List is empty only contains auxiliar element
+    if (this.props.notes.length === 1) {
+      return "List of notes is empty"
+    }
+    else {
+      const arrAux = [];
+      for( let i = 0; i < this.props.notes.length-1; i++ ) {
+        const note = this.props.notes[i].data;
+        //if element's hash and next element previous_hash are the same, note is OK
+        if (this.props.notes[i].header.hash === this.props.notes[i+1].header.previous_hash ){
+          arrAux.push(
+            (<div onClick = { () => this.props.selectItem(i) } className = "NoteOK" key={ i } >
+              <h3>{note.title}</h3>
+              <p>{note.description}</p>
+              <p>{note.created_at}</p>
+            </div>)
+          );
+        } else { //if element's hash and next element previous_hash are different, note is not OK
+          arrAux.push(
+            (<div onClick = { this.props.selectItem } className = "NoteKO" key={ i } >
+              <h3>{note.title}</h3>
+              <p>{note.description}</p>
+              <p>{note.created_at}</p>
+            </div>)
+          );
+        }
+      }
+      return arrAux;
+    }
 
+  }
   render() {
     return (
       <div className="NoteList">
-        <p>Note List</p>
+        {this.renderNotes()}
       </div>
     );
   }
