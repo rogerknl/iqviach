4d1b983b67ae5c2ebadf69f7b51cee94c3bdae20
Roger KNL <roger.knl@gmail.com>
2019-01-28 21:54:49 +0100

    structure

---
+++ src/components/NoteAdd.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './NoteAdd.css';
+
+class NoteAdd extends Component {
+
+  render() {
+    return (
+      <div className="NoteAdd">
+        <p>Note Add</p>
+      </div>
+    );
+  }
+}
+
+export default NoteAdd;
---
+++ src/components/NoteModify.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './NoteModify.css';
+
+class NoteModify extends Component {
+
+  render() {
+    return (
+      <div className="NoteModify">
+        <p>Note Modify</p>
+      </div>
+    );
+  }
+}
+
+export default NoteModify;
